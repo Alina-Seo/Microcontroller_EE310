fb9671b226139115484c6d34617bb88769603a92
Alina-Seo <alinamatchette@gmail.com>
2025-04-10 00:09:28 -0700

    Update RPNMAIN.c to v 1.2
    change port b to port c

--- Assignment/RPN Calculator/RPNMAIN.c
+++ Assignment/RPN Calculator/RPNMAIN.c
@@ -13,6 +13,7 @@
  * Star - stack clear
  * Version - 1.0 original
  * Version - 1.1  - updated output function to return a value rather than set value in stack. this allows for original unfiltered output value to remain in the stack
+ * Version - 1.2 - changes PORTB to PORTC
  * Created on April 7, 2025
  */
 
@@ -55,7 +56,7 @@ int ctrl[5] = {0,0,0,0,0}; //control array, first digit is operator, second digi
 
 
 void main(void) {
-    PORTB = 0; LATB = 0; ANSELB = 0; TRISB = 0b11110000; // port setup, ports B 4,5,6,7 as input rest of B and D as out
+    PORTC = 0; LATC = 0; ANSELC = 0; TRISC = 0b11110000; // port setup, ports C 4,5,6,7 as input rest of B and D as out
     PORTD = 0; LATD = 0; ANSELD = 0; TRISD = 0;
     
     while (1){ //main loop
@@ -136,9 +137,9 @@ void stackc(int s[], int c[]){ //clears stack and resets all values
 }
 void takeIn(int s[], int c[]){ //loosely based on the keypad input assembly code, taking input based on C and R values
     
-    PORTBbits.RB3 = 0;
-    PORTBbits.RB0 = 1;
-    if (PORTBbits.RB4 ==1){
+    PORTCbits.RC3 = 0;
+    PORTCbits.RC0 = 1;
+    if (PORTCbits.RC4 ==1){
         switch(c[1]){
             case 0: 
                 s[0] = 1;
@@ -152,7 +153,7 @@ void takeIn(int s[], int c[]){ //loosely based on the keypad input assembly code
         }
         return;
     }
-    else if (PORTBbits.RB5 ==1){
+    else if (PORTCbits.RC5 ==1){
         switch(c[1]){
             case 0: 
                 s[0] = 4;
@@ -166,7 +167,7 @@ void takeIn(int s[], int c[]){ //loosely based on the keypad input assembly code
         }
         return;
     }
-    else if (PORTBbits.RB6 ==1){
+    else if (PORTCbits.RC6==1){
         switch(c[1]){
             case 0: 
                 s[0] = 7;
@@ -180,13 +181,13 @@ void takeIn(int s[], int c[]){ //loosely based on the keypad input assembly code
         }
         return;
     }
-    else if (PORTBbits.RB7 ==1){
+    else if (PORTCbits.RC7 ==1){
         c[0] = 6;
         return;
     }
-    PORTBbits.RB0 = 0;
-    PORTBbits.RB1 = 1;
-    if (PORTBbits.RB4 ==1){
+    PORTCbits.RC0 = 0;
+    PORTCbits.RC1 = 1;
+    if (PORTCbits.RC4 ==1){
         switch(c[1]){
             case 0: 
                 s[0] = 2;
@@ -200,7 +201,7 @@ void takeIn(int s[], int c[]){ //loosely based on the keypad input assembly code
         }
         return;
     }
-    else if (PORTBbits.RB5 ==1){
+    else if (PORTCbits.RC5 ==1){
         switch(c[1]){
             case 0: 
                 s[0] = 5;
@@ -214,7 +215,7 @@ void takeIn(int s[], int c[]){ //loosely based on the keypad input assembly code
         }
         return;
     }
-    else if (PORTBbits.RB6 ==1){
+    else if (PORTCbits.RC6 ==1){
         switch(c[1]){
             case 0: 
                 s[0] = 8;
@@ -228,7 +229,7 @@ void takeIn(int s[], int c[]){ //loosely based on the keypad input assembly code
         }
         return;
     }
-    else if (PORTBbits.RB7 ==1){
+    else if (PORTCbits.RC7 ==1){
         switch(c[1]){
             case 0: 
                 s[0] = 0;
@@ -241,9 +242,9 @@ void takeIn(int s[], int c[]){ //loosely based on the keypad input assembly code
         }
         return;
     }
-    PORTBbits.RB1 = 0;
-    PORTBbits.RB2 = 1;
-    if (PORTBbits.RB4 ==1){
+    PORTCbits.RC1 = 0;
+    PORTCbits.RC2 = 1;
+    if (PORTCbits.RC4 ==1){
         switch(c[1]){
             case 0: 
                 s[0] = 3;
@@ -257,7 +258,7 @@ void takeIn(int s[], int c[]){ //loosely based on the keypad input assembly code
         }
         return;
     }
-    else if (PORTBbits.RB5 ==1){
+    else if (PORTCbits.RC5 ==1){
         switch(c[1]){
             case 0: 
                 s[0] = 6;
@@ -271,7 +272,7 @@ void takeIn(int s[], int c[]){ //loosely based on the keypad input assembly code
         }
         return;
     }
-    else if (PORTBbits.RB6 ==1){
+    else if (PORTCbits.RC6 ==1){
         switch(c[1]){
             case 0: 
                 s[0] = 9;
@@ -285,25 +286,25 @@ void takeIn(int s[], int c[]){ //loosely based on the keypad input assembly code
         }
         return;
     }
-    else if (PORTBbits.RB7 ==1){
+    else if (PORTCbits.RC7 ==1){
         c[0] = 5;
         return;
     }
-    PORTBbits.RB2 = 0;
-    PORTBbits.RB3 = 1;
-    if (PORTBbits.RB4 ==1){
+    PORTCbits.RC2 = 0;
+    PORTCbits.RC3 = 1;
+    if (PORTCbits.RC4 ==1){
         c[0] = 1;
         return;
     }
-    else if (PORTBbits.RB5 ==1){
+    else if (PORTCbits.RC5 ==1){
         c[0] = 2;
         return;
     }
-    else if (PORTBbits.RB6 ==1){
+    else if (PORTCbits.RC6 ==1){
         c[0] = 3;
         return;
     }
-    else if (PORTBbits.RB7 ==1){
+    else if (PORTCbits.RC7 ==1){
         c[0] = 4;
         return;
     }
