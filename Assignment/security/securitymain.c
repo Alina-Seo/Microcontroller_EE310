77af8d349dcb434166cf932a353886280c0f3974
Alina-Seo <alinamatchette@gmail.com>
2025-04-22 16:30:48 -0700

    Update securitymain.c to 1.1
    moved more functions off to separate dependency files

--- Assignment/security/securitymain.c
+++ Assignment/security/securitymain.c
@@ -10,60 +10,36 @@
  * 
  * outputs: beeper connected to C6, motor connected to C5 through motor controller
  * 
- * dependencies: functions.h, xc.h (on git repo)
- * Version 1.0
+ * dependencies: functions.h, xc.h,initialize.h (on git repo)
+ * Version 1.0 - original
+ * Version 1.1 - moved more functions to the functions file and now added initialize file 
  * Created on April 17, 2025, 1:31 PM
  */
 
 // CONFIG1L
 
 
-#include <./xc.h>
+#include "xc.h"
+#include "initialize.h"
 #include "functions.h"
 #include "C:/Program Files/Microchip/xc8/v3.00/pic/include/proc/pic18f47k42.h"
 #define _XTAL_FREQ 4000000                 // Fosc  frequency for _delay()  library
 #define FCY    _XTAL_FREQ/4
-#pragma config WDTE = OFF
 
-void __interrupt(irq(IRQ_INT0),base(0x4008)) INT0_ISR(void) //code for interupt, when triggered beeps speaker 3 times
-{
-    if (PIR1bits.INT0IF == 1){
-        for (int i =1;i<= 6;i++){
-        PORTCbits.RC6  = !PORTCbits.RC6;
-        __delay_ms(250);
-        }
-       PIR1bits.INT0IF =0;
-       PORTCbits.RC6 = 0;     
-    }
-    
-}
 
-void INTERRUPT_Initialize (void) //setup and init for interupt
-{
 
-    INTCON0bits.IPEN = 1;
-    INTCON0bits.GIEH = 1;
-    INTCON0bits.GIEL = 1;
-    INTCON0bits.INT0EDG = 1;
-    IPR1bits.INT0IP = 1;
-    PIE1bits.INT0IE = 1;
-    PIR1bits.INT0IF =0; 
 
-    IVTBASEU = 0x00;
-    IVTBASEH = 0x40;
-    IVTBASEL = 0x08;
-            
-}
+
 
 int code[2] = {2,3}; //setting up both the code and user entry arrays
 int usercode[2];
 
 void main(void) {
-    PORTD = 0; LATD = 0; ANSELD = 0; TRISD = 0; //port setup and innit
-    PORTC = 0; LATC = 0; ANSELC = 0; TRISC = 0x00011111;
-    PORTCbits.RC6 = 0; 
-    PORTDbits.RD7 = 1; //always on LED connected and turned on here
+    
     INTERRUPT_Initialize();
+    init();
+     //always on LED connected and turned on here
+    
 while (1){
     
     if (PORTCbits.RC2 == 1){ //check first digit switch
@@ -72,6 +48,7 @@ while (1){
         usercode[0] = 0;
         }
     PORTD = sevenseg(usercode[0]); //outputs number in the reg to the display
+    PORTDbits.RD7 = 1;
 }
     else if (PORTCbits.RC3 == 1){ //check second digit switch
         usercode[1] += 1;
@@ -79,6 +56,7 @@ while (1){
         usercode[1] = 0;
         }
         PORTD = sevenseg(usercode[1]);
+        PORTDbits.RD7 = 1;
     }
     else if (PORTCbits.RC4 == 1){ //when enter button pressed, checks arrays and 
         if (code[0] ==  usercode[0] &&code[1] ==  usercode[1]){
@@ -87,8 +65,9 @@ while (1){
         PORTCbits.RC5 = 0;
         }
         else{ //beep if not
+        
         PORTCbits.RC6 = 1;
-        __delay_ms(500);
+        __delay_ms(300);
         PORTCbits.RC6 = 0;
         }
         
